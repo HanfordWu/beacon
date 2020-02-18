e56162882e8c87c5f1ae1a1ddae36ee6fb9fee20
Tristan Struthers (trstruth) <trstruth@nettools1-co1.phx.gbl>
2020-02-18 19:28:29 +0000

    Refactor and add some reference comments

--- beacon.go
+++ beacon.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net"
 	"syscall"
@@ -10,24 +11,26 @@ import (
 )
 
 const (
-        IP_HEADER_LEN = 20
-        ICMP_HEADER_LEN = 8
-        PAYLOAD_LEN = 20
+        ipHeaderLen = 20
+        icmpHeaderLen = 8
 )
 
 func main() {
 	sourceIP := net.IPv4(10, 20, 30, 96)
 	destIP := net.IPv4(104, 44, 227, 112)
+	
+	payload := []byte{'H', 'e', 'l', 'l', 'o'}
 
 	buf := gopacket.NewSerializeBuffer()
 	opts := gopacket.SerializeOptions{
 		ComputeChecksums: true,
 	}
 
+	// outer ip in ip header (layer 3) https://tools.ietf.org/html/rfc2003#section-3
 	ipipLayer := &layers.IPv4{
 		Version:  4,
 		IHL:      5,
-		Length:   IP_HEADER_LEN + IP_HEADER_LEN + ICMP_HEADER_LEN + PAYLOAD_LEN,
+		Length:   uint16(ipHeaderLen + ipHeaderLen + icmpHeaderLen + len(payload)),
 		Flags:    layers.IPv4DontFragment,
 		TTL:      64,
 		Protocol: layers.IPProtocolIPv4,
@@ -35,10 +38,11 @@ func main() {
 		DstIP:    destIP,
 	}
 
+	// inner ip header (layer 3) https://tools.ietf.org/html/rfc791#section-3.1
 	ipLayer := &layers.IPv4{
 		Version:  4,
 		IHL:      5,
-		Length:   IP_HEADER_LEN + ICMP_HEADER_LEN + PAYLOAD_LEN,
+		Length:   uint16(ipHeaderLen + icmpHeaderLen + len(payload)),
 		Flags:    layers.IPv4DontFragment,
 		TTL:      64,
 		Protocol: layers.IPProtocolICMPv4,
@@ -46,6 +50,7 @@ func main() {
 		DstIP:    sourceIP,
 	}
 
+	// inner icmp header (layer 4) https://tools.ietf.org/html/rfc792#page-4
 	icmpLayer := &layers.ICMPv4{
 		TypeCode: layers.CreateICMPv4TypeCode(layers.ICMPv4TypeEchoRequest, 0),
 		Seq:      1,
@@ -55,30 +60,36 @@ func main() {
 		ipipLayer,
 		ipLayer,
 		icmpLayer,
-                gopacket.Payload([]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}),
+        gopacket.Payload(payload),
 	)
 	if err != nil {
 		log.Fatalf("Error serializing packet: %s", err)
 	}
 	packetData := buf.Bytes()
 
-	err = sendPacketSyscall(packetData)
+	err = sendPacket(packetData, destIP)
 	if err != nil {
-		log.Fatal("Sendto:", err)
+		log.Fatal("Failed to call send packet: ", err)
 	}
-
-	log.Println("Successfully sent a packet")
+	log.Printf("Successfully sent a packet: %v\n", packetData)
 }
 
-func sendPacketSyscall(packetData []byte) error {
+func sendPacket(packetData []byte, dest net.IP) error {
+	// open a raw socket, the IPPROTO_RAW protocol implies IP_HDRINCL is enabled
+	// http://man7.org/linux/man-pages/man7/raw.7.html
 	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_RAW)
 	if err != nil {
 		return err
 	}
 	defer syscall.Close(fd)
 
+	dest = dest.To4()
+	if dest == nil {
+		return errors.New("dest IP must be an ipv4 address")
+	}
+
 	addr := syscall.SockaddrInet4{
-		Addr: [4]byte{104, 44, 227, 112},
+		Addr: [4]byte{dest[0], dest[1], dest[2], dest[3]},
 	}
 	return syscall.Sendto(fd, packetData, 0, &addr)
 }
