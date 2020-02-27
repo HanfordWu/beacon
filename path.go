2508061235412a12c54aacf9bf4b71ef592d0cb2
Tristan Struthers <trstruth@microsoft.com>
2020-02-27 00:11:34 +0000

    Reimplement path functions with channels, and create accumulator wrappers

--- path.go
+++ path.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net"
 
 	"github.com/google/gopacket"
@@ -16,52 +15,15 @@ type PathChannel chan net.IP
 
 // GetPathTo returns a Path to a destination IP from the caller
 func GetPathTo(destIP net.IP, tc TransportChannel) (Path, error) {
-	path := make(Path, 0)
+	path := make([]net.IP, 0)
 
-	done := make(chan error)
-	found := make(chan uint8)
-
-	sourceIP, err := findLocalIP()
+	pc, err := GetPathChannelTo(destIP, tc)
 	if err != nil {
 		return path, err
 	}
 
-	go func() {
-		buf := gopacket.NewSerializeBuffer()
-
-		var ttl uint8
-		for ttl = 0; ttl <= 32; ttl++ {
-			err = buildICMPTraceroutePacket(sourceIP, destIP, ttl, []byte("Hello"), buf)
-			if err != nil {
-				done <- err
-			}
-			tc.SendTo(buf.Bytes(), destIP)
-			<-found
-		}
-	}()
-
-	go func() {
-		for packet := range tc.Rx() {
-			icmpLayer := packet.Layer(layers.LayerTypeICMPv4)
-			ipv4Layer := packet.Layer(layers.LayerTypeIPv4)
-			icmp, _ := icmpLayer.(*layers.ICMPv4)
-			ip4, _ := ipv4Layer.(*layers.IPv4)
-
-			// fmt.Printf("%s -> %s : %s\n", ip4.SrcIP, ip4.DstIP, icmp.TypeCode)
-			if int(icmp.TypeCode) == icmpTTLExceeded && ip4.DstIP.Equal(sourceIP) {
-				path = append(path, ip4.SrcIP)
-				found <- ip4.TTL
-			} else if int(icmp.TypeCode) == icmpEchoReply && ip4.SrcIP.Equal(destIP) {
-				path = append(path, ip4.DstIP)
-				found <- ip4.TTL
-				done <- nil
-			}
-		}
-	}()
-
-	err = <-done
-	if err != nil {
-		return path, err
+	for hop := range pc {
+		path = append(path, hop)
 	}
 
 	return path, nil
@@ -69,59 +31,15 @@ func GetPathTo(destIP net.IP, tc TransportChannel) (Path, error) {
 
 // GetPathFrom returns a Path from a destination IP back to the caller
 func GetPathFrom(destIP net.IP, tc TransportChannel) (Path, error) {
-	path := make(Path, 0)
+	path := make([]net.IP, 0)
 
-	done := make(chan error)
-	found := make(chan uint8)
-
-	sourceIP, err := findLocalIP()
+	pc, err := GetPathChannelFrom(destIP, tc)
 	if err != nil {
 		return path, err
 	}
 
-	go func() {
-		roundTripBuf := gopacket.NewSerializeBuffer()
-		remoteProbeBuf := gopacket.NewSerializeBuffer()
-
-		var ttl uint8
-		for ttl = 0; ttl <= 32; ttl++ {
-			err = buildEncapTraceroutePacket(sourceIP, destIP, sourceIP, sourceIP, ttl, []byte("Hello"), roundTripBuf)
-			if err != nil {
-				done <- err
-			}
-			err = buildEncapTraceroutePacket(sourceIP, destIP, destIP, sourceIP, ttl+1, []byte("Hello"), remoteProbeBuf)
-			if err != nil {
-				done <- err
-			}
-			tc.SendTo(roundTripBuf.Bytes(), destIP)
-			tc.SendTo(remoteProbeBuf.Bytes(), destIP)
-			<-found
-		}
-	}()
-
-	go func() {
-		for packet := range tc.Rx() {
-			// TODO: consider using DecodingLayerParser https://godoc.org/github.com/google/gopacket#hdr-Fast_Decoding_With_DecodingLayerParser
-			icmpLayer := packet.Layer(layers.LayerTypeICMPv4)
-			ipv4Layer := packet.Layer(layers.LayerTypeIPv4)
-			icmp, _ := icmpLayer.(*layers.ICMPv4)
-			ip4, _ := ipv4Layer.(*layers.IPv4)
-
-			// fmt.Printf("%s -> %s : %s\n", ip4.SrcIP, ip4.DstIP, icmp.TypeCode)
-			if int(icmp.TypeCode) == icmpTTLExceeded && ip4.DstIP.Equal(sourceIP) {
-				path = append(path, ip4.SrcIP)
-				found <- ip4.TTL
-			} else if int(icmp.TypeCode) == icmpEchoRequest && ip4.SrcIP.Equal(destIP) {
-				path = append(path, ip4.DstIP)
-				found <- ip4.TTL
-				done <- nil
-			}
-		}
-	}()
-
-	err = <-done
-	if err != nil {
-		return path, err
+	for hop := range pc {
+		path = append(path, hop)
 	}
 
 	return path, nil
@@ -131,33 +49,107 @@ func GetPathFrom(destIP net.IP, tc TransportChannel) (Path, error) {
 func GetPathFromSourceToDest(sourceIP, destIP net.IP, tc TransportChannel) (Path, error) {
 	path := make([]net.IP, 0)
 
-	found := make(chan uint8)
+	pc, err := GetPathChannelFromSourceToDest(sourceIP, destIP, tc)
+	if err != nil {
+		return path, err
+	}
+
+	for hop := range pc {
+		path = append(path, hop)
+	}
+
+	return path, nil
+}
+
+// GetPathChannelTo returns a PathChannel to a destination IP from the caller
+func GetPathChannelTo(destIP net.IP, tc TransportChannel) (PathChannel, error) {
+
+	pathChan := make(PathChannel)
+	found := make(chan net.IP)
+	done := make(chan error)
+
+	sourceIP, err := findLocalIP()
+	if err != nil {
+		return pathChan, err
+	}
+
+	go func() {
+		defer close(pathChan)
+		buf := gopacket.NewSerializeBuffer()
+
+		var ttl uint8
+		for ttl = 1; ttl <= 32; ttl++ {
+			err = buildICMPTraceroutePacket(sourceIP, destIP, ttl, []byte("Hello"), buf)
+			if err != nil {
+				done <- err
+			}
+			tc.SendTo(buf.Bytes(), destIP)
+			select {
+			case ip := <-found:
+				pathChan <- ip
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	go func() {
+		for packet := range tc.Rx() {
+			icmpLayer := packet.Layer(layers.LayerTypeICMPv4)
+			ipv4Layer := packet.Layer(layers.LayerTypeIPv4)
+			icmp, _ := icmpLayer.(*layers.ICMPv4)
+			ip4, _ := ipv4Layer.(*layers.IPv4)
+
+			// fmt.Printf("%s -> %s : %s\n", ip4.SrcIP, ip4.DstIP, icmp.TypeCode)
+			if int(icmp.TypeCode) == icmpTTLExceeded && ip4.DstIP.Equal(sourceIP) {
+				found <- ip4.SrcIP
+			} else if int(icmp.TypeCode) == icmpEchoReply && ip4.SrcIP.Equal(destIP) {
+				found <- ip4.SrcIP
+				done <- nil
+				return
+			}
+		}
+	}()
+
+	return pathChan, nil
+}
+
+// GetPathChannelFrom returns a PathChannel from a destination IP back to the caller
+func GetPathChannelFrom(destIP net.IP, tc TransportChannel) (PathChannel, error) {
+	pathChan := make(PathChannel)
+	found := make(chan net.IP)
 	done := make(chan error)
 
 	localIP, err := findLocalIP()
 	if err != nil {
-		return path, err
-	}
-	if localIP.Equal(sourceIP) {
-		return GetPathTo(destIP, tc)
-	}
-	if localIP.Equal(destIP) {
-		return GetPathFrom(destIP, tc)
+		return pathChan, err
 	}
 
 	go func() {
+		defer close(pathChan)
+		roundTripBuf := gopacket.NewSerializeBuffer()
+		remoteProbeBuf := gopacket.NewSerializeBuffer()
 
 		var ttl uint8
-		for ttl = 0; ttl <= 32; ttl++ {
-			buf := gopacket.NewSerializeBuffer()
-			payload := []byte("Hello")
+		for ttl = 1; ttl <= 32; ttl++ {
+			err = buildEncapTraceroutePacket(localIP, destIP, localIP, localIP, ttl, []byte("Hello"), roundTripBuf)
+			if err != nil {
+				done <- err
+			}
+			err = buildEncapTraceroutePacket(localIP, destIP, destIP, localIP, ttl+1, []byte("Hello"), remoteProbeBuf)
+			if err != nil {
+				done <- err
+			}
+			tc.SendTo(roundTripBuf.Bytes(), destIP)
+			tc.SendTo(remoteProbeBuf.Bytes(), destIP)
 
-			buildEncapTraceroutePacket(localIP, sourceIP, localIP, destIP, ttl, payload, buf)
-
-			tc.SendTo(buf.Bytes(), sourceIP)
-			<-found
+			select {
+			case ip := <-found:
+				pathChan <- ip
+			case <-done:
+				return
+			}
 		}
-
 	}()
 
 	go func() {
@@ -168,21 +160,67 @@ func GetPathFromSourceToDest(sourceIP, destIP net.IP, tc TransportChannel) (Path
 			icmp, _ := icmpLayer.(*layers.ICMPv4)
 			ip4, _ := ipv4Layer.(*layers.IPv4)
 
-			fmt.Printf("%s -> %s : %s\n", ip4.SrcIP, ip4.DstIP, icmp.TypeCode)
+			// fmt.Printf("%s -> %s : %s\n", ip4.SrcIP, ip4.DstIP, icmp.TypeCode)
 			if int(icmp.TypeCode) == icmpTTLExceeded && ip4.DstIP.Equal(localIP) {
-				path = append(path, ip4.SrcIP)
-				found <- ip4.TTL
-			} else if int(icmp.TypeCode) == icmpEchoReply && ip4.SrcIP.Equal(destIP) {
-				path = append(path, ip4.DstIP)
-				found <- ip4.TTL
+				found <- ip4.SrcIP
+			} else if int(icmp.TypeCode) == icmpEchoRequest && ip4.SrcIP.Equal(destIP) {
+				found <- ip4.DstIP
 				done <- nil
+				return
 			}
 		}
 	}()
 
-	err = <-done
+	return pathChan, nil
+}
+
+// GetPathChannelFromSourceToDest returns a PathChannel from a sourceIP to a destIP
+func GetPathChannelFromSourceToDest(sourceIP, destIP net.IP, tc TransportChannel) (PathChannel, error) {
+	pathChan := make(PathChannel)
+	found := make(chan net.IP)
+	done := make(chan error)
+
+	localIP, err := findLocalIP()
 	if err != nil {
-		return path, err
+		return pathChan, err
 	}
-	return path, nil
+
+	go func() {
+		defer close(pathChan)
+		var ttl uint8
+		for ttl = 1; ttl <= 32; ttl++ {
+			buf := gopacket.NewSerializeBuffer()
+			payload := []byte("Hello")
+
+			buildEncapTraceroutePacket(localIP, sourceIP, localIP, destIP, ttl, payload, buf)
+
+			tc.SendTo(buf.Bytes(), sourceIP)
+			
+			select {
+			case ip := <-found:
+				pathChan <- ip
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	go func() {
+		for packet := range tc.Rx() {
+			icmpLayer := packet.Layer(layers.LayerTypeICMPv4)
+			ipv4Layer := packet.Layer(layers.LayerTypeIPv4)
+			icmp, _ := icmpLayer.(*layers.ICMPv4)
+			ip4, _ := ipv4Layer.(*layers.IPv4)
+
+			if int(icmp.TypeCode) == icmpTTLExceeded && ip4.DstIP.Equal(localIP) {
+				found <- ip4.SrcIP
+			} else if int(icmp.TypeCode) == icmpEchoReply && ip4.SrcIP.Equal(destIP) {
+				found <- ip4.SrcIP
+				done <- nil
+				return
+			}
+		}
+	}()
+
+	return pathChan, nil
 }
