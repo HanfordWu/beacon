8718b731f6a595f069301836987bce0a60588338
Tristan Struthers <trstruth@microsoft.com>
2020-02-27 01:10:47 +0000

    Gut cli skeleton

--- cmd/beacon.go
+++ cmd/beacon.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-	"log"
-	"net"
-
-	"github.com/trstruth/beacon"
+	"fmt"
 )
 
 func main() {
-	// sourceIP := net.IP{104, 44, 227, 112}
-	destIP := net.IP{207, 46, 33, 149}
-
-	tc, err := NewTransportChannel(WithBPFFilter("icmp"))
-	if err != nil {
-		log.Fatalf("Failed to create new TransportChannel: %s", err)
-	}
-
-	err = Spray(destIP, *tc)
-	if err != nil {
-		log.Fatalf("Spray failed: %s", err)
-	}
+	fmt.Println("beacon CLI")
 }
