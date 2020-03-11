0b7b0916beabfa836629762c3fbb0b4bd0036ceb
trstruth <trstruth@microsoft.com>
2020-03-11 14:14:18 -0700

    Add traceroute and rtraceroute functionality to braceroute cli

--- braceroute/braceroute.go
+++ braceroute/braceroute.go
@@ -1,30 +1,55 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"net"
 	"os"
 
 	"github.com/spf13/cobra"
 )
+
+var reverse bool
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "braceroute",
 	Short: "the beacon of gondor has been lit",
 	Long:  "Localize network failures using IP in IP encapsulation",
-}
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			cmd.Help()
+			return errors.New("")
+		}
 
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the version number of braceroute",
-	Long:  `All software has versions. This is braceroute's`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.1.0")
+		if len(args) > 1 {
+			return fmt.Errorf("Expected one argument, got %d", len(args))
+		}
+
+		return nil
 	},
+	RunE:    bracerouteRun,
+	Version: "v0.1.0",
 }
 
 func init() {
-	RootCmd.AddCommand(versionCmd)
+	RootCmd.Flags().BoolVarP(&reverse, "reverse", "r", false, "trace the route in reverse from target back to caller")
+}
+
+func bracerouteRun(cmd *cobra.Command, args []string) error {
+	destIP := net.ParseIP(args[0])
+
+	if reverse {
+		if err := ReverseTraceroute(destIP); err != nil {
+			return err
+		}
+	} else {
+		if err := Traceroute(destIP); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
---
+++ braceroute/traceroute.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/trstruth/beacon"
+)
+
+// Traceroute performs traditional traceroute
+func Traceroute(destIP net.IP) error {
+	tc, err := beacon.NewTransportChannel(beacon.WithBPFFilter("icmp"))
+	if err != nil {
+		return err
+	}
+
+	destHostname, err := net.LookupAddr(destIP.String())
+	if err != nil {
+		fmt.Printf("Doing traceroute to %s\n", destIP)
+	} else {
+		fmt.Printf("Doing traceroute to %s (%s)\n", destHostname[0], destIP)
+	}
+
+	pc, err := beacon.GetPathChannelTo(destIP, *tc)
+	if err != nil {
+		return err
+	}
+
+	for hop := range pc {
+		hostname, err := net.LookupAddr(hop.String())
+		if err != nil {
+			fmt.Println(hop.String())
+		} else {
+			fmt.Println(hostname[0])
+		}
+	}
+
+	return nil
+}
