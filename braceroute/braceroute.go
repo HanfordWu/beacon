f26b8b496baea480a0928cfc17e5e0aab56f45c5
Tristan Struthers <trstruth@microsoft.com>
2020-03-12 20:25:30 +0000

    cobra related structural refactor

--- braceroute/braceroute.go
+++ braceroute/braceroute.go
@@ -1,55 +1,17 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net"
 	"os"
-
-	"github.com/spf13/cobra"
 )
-
-var reverse bool
-
-// RootCmd represents the base command when called without any subcommands
-var RootCmd = &cobra.Command{
-	Use:   "braceroute",
-	Short: "the beacon of gondor has been lit",
-	Long:  "Localize network failures using IP in IP encapsulation",
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) == 0 {
-			cmd.Help()
-			return errors.New("")
-		}
-
-		if len(args) > 1 {
-			return fmt.Errorf("Expected one argument, got %d", len(args))
-		}
-
-		return nil
-	},
-	RunE:    bracerouteRun,
-	Version: "v0.1.0",
-}
 
 func init() {
 	RootCmd.Flags().BoolVarP(&reverse, "reverse", "r", false, "trace the route in reverse from target back to caller")
-}
+	RootCmd.AddCommand(SprayCmd)
 
-func bracerouteRun(cmd *cobra.Command, args []string) error {
-	destIP := net.ParseIP(args[0])
-
-	if reverse {
-		if err := ReverseTraceroute(destIP); err != nil {
-			return err
-		}
-	} else {
-		if err := Traceroute(destIP); err != nil {
-			return err
-		}
-	}
-
-	return nil
+	SprayCmd.Flags().StringVarP(&source, "source", "s", "", "source IP/host (defaults to eth0 interface)")
+	SprayCmd.Flags().StringVarP(&dest, "dest", "d", "", "destination IP/host (required)")
+	SprayCmd.MarkFlagRequired("dest")
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
---
+++ braceroute/spray.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/spf13/cobra"
+	"github.com/trstruth/beacon"
+)
+
+var source string
+var dest string
+
+// SprayCmd represents the spray subcommand which allows a user to send
+// a spray of packets over a path from source to dest
+var SprayCmd = &cobra.Command{
+	Use: "spray",
+	Short: "spray packets over a path",
+	Long: "longer description for spraying packets over a path",
+	RunE: sprayRun,
+}
+
+func sprayRun(cmd *cobra.Command, args []string) error {
+	var err error
+	var srcIP, destIP net.IP
+
+	// if no source was provided via cli flag, default to local
+	if source == "" {
+		srcIP, err = beacon.FindLocalIP()
+	} else {
+		srcIP, err = beacon.ParseIPFromString(source)
+	}
+	if err != nil {
+		return err
+	}
+
+	destIP, err = beacon.ParseIPFromString(dest)
+	if err != nil {
+		return err
+	}
+
+	path, err := beacon.GetPathFromSourceToDest(srcIP, destIP)
+	if err != nil {
+		return err
+	}
+
+	// if the caller isn't the host, prepend the host to the path
+	if source != "" {
+		vantageIP, err := beacon.FindLocalIP()
+		if err != nil {
+			return err
+		}
+		path = append([]net.IP{vantageIP}, path...)
+	}
+
+	tc, err := beacon.NewTransportChannel(beacon.WithBPFFilter("ip proto 4"))
+	if err != nil {
+		return err
+	}
+
+	return spray(path, tc)
+}
+
+func spray(path beacon.Path, tc *beacon.TransportChannel) error {
+	payload := []byte("boomerang mode")
+	buf := gopacket.NewSerializeBuffer()
+
+	err := beacon.CreateRoundTripPacketForPath(path, payload, buf)
+	if err != nil {
+		return err
+	}
+
+	done := make(chan error)
+
+	go func() {
+		for packet := range tc.Rx() {
+			udpLayer := packet.Layer(layers.LayerTypeUDP)
+			ipv4Layer := packet.Layer(layers.LayerTypeIPv4)
+			udp, _ := udpLayer.(*layers.UDP)
+			ip4, _ := ipv4Layer.(*layers.IPv4)
+
+			if ip4.DstIP.Equal(path[0]) && ip4.SrcIP.Equal(path[1]) {
+				fmt.Printf("%s -> %s: %s\n", path[1], path[0], udp.Payload)
+				done <- nil
+			}
+		}
+	}()
+
+	fmt.Println("sending packet")
+	err = tc.SendToPath(buf.Bytes(), path)
+	if err != nil {
+		return err
+	}
+
+	return <-done
+}
