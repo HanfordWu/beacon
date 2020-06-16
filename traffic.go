017264ed6991f2820dc579fe12ad189cb4ef9f84
Tristan Struthers <trstruth@microsoft.com>
2020-06-16 20:31:37 +0000

    Refactor traffic code to use listeners

--- traffic.go
+++ traffic.go
@@ -37,7 +37,6 @@ func NewBoomerangPayload(destIP net.IP, id string) *BoomerangPayload {
 		DestIP: destIP,
 		ID:     id,
 	}
-
 }
 
 // BoomerangErrorType is an enum of possible errors encountered during a run of boomerang
@@ -60,17 +59,19 @@ func (b *BoomerangResult) IsFatal() bool {
 // returned channel
 func ProbeEachHopOfPath(path Path, interfaceDevice string, numPackets int, timeout int) <-chan BoomerangResult {
 	resultChannels := make([]chan BoomerangResult, len(path)-1)
+
+	tc, err := NewTransportChannel(
+		WithBPFFilter("ip proto 4"),
+		WithInterface(interfaceDevice),
+		WithTimeout(100),
+	)
+	if err != nil {
+		resultChan := make(chan BoomerangResult)
+		resultChan <- BoomerangResult{Err: err, ErrorType: fatal}
+		return resultChan
+	}
+
 	for i := 2; i <= len(path); i++ {
-		tc, err := NewTransportChannel(
-			WithBPFFilter("ip proto 4"),
-			WithInterface(interfaceDevice),
-			WithTimeout(100),
-		)
-		if err != nil {
-			resultChan := make(chan BoomerangResult)
-			resultChan <- BoomerangResult{Err: err, ErrorType: fatal}
-			return resultChan
-		}
 		resultChannels[i-2] = Probe(path[0:i], tc, numPackets, timeout)
 	}
 
@@ -82,20 +83,14 @@ func ProbeEachHopOfPath(path Path, interfaceDevice string, numPackets int, timeo
 func ProbeEachHopOfPathSync(path Path, interfaceDevice string, numPackets int, timeout int) <-chan BoomerangResult {
 	resultChan := make(chan BoomerangResult)
 
-	transportChannels := make([]*TransportChannel, len(path)-1)
-
-	// initialize transport channels
-	for i := 2; i <= len(path); i++ {
-		tc, err := NewTransportChannel(
-			WithBPFFilter("ip proto 4"),
-			WithInterface(interfaceDevice),
-			WithTimeout(100),
-		)
-		if err != nil {
-			resultChan <- BoomerangResult{Err: err, ErrorType: fatal}
-			return resultChan
-		}
-		transportChannels[i-2] = tc
+	tc, err := NewTransportChannel(
+		WithBPFFilter("ip proto 4"),
+		WithInterface(interfaceDevice),
+		WithTimeout(100),
+	)
+	if err != nil {
+		resultChan <- BoomerangResult{Err: err, ErrorType: fatal}
+		return resultChan
 	}
 
 	go func() {
@@ -106,7 +101,7 @@ func ProbeEachHopOfPathSync(path Path, interfaceDevice string, numPackets int, t
 
 			for i := 2; i <= len(path); i++ {
 				go func(idx int) {
-					resultChan <- Boomerang(path[0:idx], transportChannels[idx-2], timeout)
+					resultChan <- Boomerang(path[0:idx], tc, timeout)
 					wg.Done()
 				}(i)
 			}
@@ -119,9 +114,9 @@ func ProbeEachHopOfPathSync(path Path, interfaceDevice string, numPackets int, t
 	return resultChan
 }
 
-// ProbeEachHopOfPathShared probes each hop in a path, but accepts a transport channel as an argument.  This allows the caller to share
+// ProbeEachHopOfPathWithTC probes each hop in a path, but accepts a transport channel as an argument.  This allows the caller to share
 // one transport channel between many calls to Probe.  The supplied tranport channel must have a BPFFilter of "ip proto 4"
-func ProbeEachHopOfPathShared(tc *TransportChannel, path Path, numPackets int, timeout int) <-chan BoomerangResult {
+func ProbeEachHopOfPathWithTC(path Path, tc *TransportChannel, numPackets int, timeout int) <-chan BoomerangResult {
 	if !strings.Contains(tc.filter, "ip proto 4") {
 		resultChan := make(chan BoomerangResult)
 
@@ -135,7 +130,7 @@ func ProbeEachHopOfPathShared(tc *TransportChannel, path Path, numPackets int, t
 
 	resultChannels := make([]chan BoomerangResult, len(path)-1)
 	for i := 2; i <= len(path); i++ {
-		resultChannels[i-2] = ProbeShared(path[0:i], tc, numPackets, timeout)
+		resultChannels[i-2] = Probe(path[0:i], tc, numPackets, timeout)
 	}
 
 	return merge(resultChannels...)
@@ -144,40 +139,10 @@ func ProbeEachHopOfPathShared(tc *TransportChannel, path Path, numPackets int, t
 // Probe generates traffic over a given path and returns a channel of boomerang results
 func Probe(path Path, tc *TransportChannel, numPackets int, timeout int) chan BoomerangResult {
 	resultChan := make(chan BoomerangResult)
-	var err error
 
 	go func() {
 		for i := 1; i <= numPackets; i++ {
 			result := Boomerang(path, tc, timeout)
-			if result.Err != nil && (result.ErrorType == timedOut || result.ErrorType == sendError) {
-				go tc.Close()
-				tc, err = NewTransportChannel(
-					WithBPFFilter(tc.filter),
-					WithInterface(tc.deviceName),
-					WithTimeout(tc.timeout),
-				)
-				if err != nil {
-					resultChan <- BoomerangResult{
-						Err:       err,
-						ErrorType: fatal,
-					}
-					return
-				}
-			}
-			resultChan <- result
-		}
-		close(resultChan)
-	}()
-
-	return resultChan
-}
-
-func ProbeShared(path Path, tc *TransportChannel, numPackets int, timeout int) chan BoomerangResult {
-	resultChan := make(chan BoomerangResult)
-
-	go func() {
-		for i := 1; i <= numPackets; i++ {
-			result := BoomerangShared(path, tc, timeout)
 			resultChan <- result
 		}
 		close(resultChan)
@@ -189,98 +154,6 @@ func ProbeShared(path Path, tc *TransportChannel, numPackets int, timeout int) c
 // Boomerang sends one packet which "boomerangs" over a given path.  For example, if the path is A,B,C,D the packet will travel
 // A -> B -> C -> D -> C -> B -> A
 func Boomerang(path Path, tc *TransportChannel, timeout int) BoomerangResult {
-	listenerReady := make(chan bool)
-	seen := make(chan BoomerangResult)
-	resultChan := make(chan BoomerangResult)
-
-	destHop := path[len(path)-1]
-	id := uuid.New().String()
-	payload, err := json.Marshal(NewBoomerangPayload(destHop, id))
-	if err != nil {
-		return BoomerangResult{
-			Err:       err,
-			ErrorType: fatal,
-		}
-	}
-
-	buf := gopacket.NewSerializeBuffer()
-	err = CreateRoundTripPacketForPath(path, payload, buf)
-	if err != nil {
-		return BoomerangResult{
-			Err:       err,
-			ErrorType: fatal,
-		}
-	}
-
-	go func() {
-		listenerReady <- true
-		for packet := range tc.Rx() {
-			udpLayer := packet.Layer(layers.LayerTypeUDP)
-			ipv4Layer := packet.Layer(layers.LayerTypeIPv4)
-			udp, _ := udpLayer.(*layers.UDP)
-			ip4, _ := ipv4Layer.(*layers.IPv4)
-
-			if ip4.DstIP.Equal(path[0]) && ip4.SrcIP.Equal(path[1]) {
-				unmarshalledPayload := &BoomerangPayload{}
-				err := json.Unmarshal(udp.Payload, unmarshalledPayload)
-				if err != nil {
-					continue
-				}
-
-				if unmarshalledPayload.ID == id {
-					unmarshalledPayload.RxTimestamp = time.Now().UTC()
-					seen <- BoomerangResult{
-						Payload: *unmarshalledPayload,
-					}
-					return
-				}
-			}
-		}
-	}()
-
-	go func() {
-		<-listenerReady
-
-		timeOutDuration := time.Duration(timeout) * time.Second
-		timer := time.NewTimer(timeOutDuration)
-
-		txTime := time.Now().UTC()
-		err := tc.SendToPath(buf.Bytes(), path)
-		if err != nil {
-			fmt.Printf("error in SendToPath: %s\n", err)
-			resultChan <- BoomerangResult{
-				Err:       err,
-				ErrorType: sendError,
-				Payload: BoomerangPayload{
-					DestIP: path[len(path)-1],
-				},
-			}
-			return
-		}
-
-		select {
-		case result := <-seen:
-			result.Payload.TxTimestamp = txTime
-			resultChan <- result
-		case <-timer.C:
-			resultChan <- BoomerangResult{
-				Payload: BoomerangPayload{
-					DestIP:      path[len(path)-1],
-					TxTimestamp: txTime,
-					RxTimestamp: time.Now().UTC(),
-				},
-				Err:       errors.New("timed out waiting for packet from " + path[len(path)-1].String()),
-				ErrorType: timedOut,
-			}
-		}
-	}()
-
-	return <-resultChan
-}
-
-// BoomerangShared sends one packet which "boomerangs" over a given path.  For example, if the path is A,B,C,D the packet will travel
-// A -> B -> C -> D -> C -> B -> A
-func BoomerangShared(path Path, tc *TransportChannel, timeout int) BoomerangResult {
 	listenerReady := make(chan bool)
 	seen := make(chan BoomerangResult)
 	resultChan := make(chan BoomerangResult)
