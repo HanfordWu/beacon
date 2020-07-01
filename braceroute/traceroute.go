1234d2fb460007b59524d372d1980f69f2250468
Shubhangi Raj <s4raj@uwaterloo.ca>
2020-07-01 15:04:10 -0400

    Added traceroute to beacon pkg

--- braceroute/traceroute.go
+++ braceroute/traceroute.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"net"
 
 	"github.com/trstruth/beacon"
@@ -9,50 +9,53 @@ import (
 
 // Traceroute performs traditional traceroute
 func Traceroute(destIP net.IP, timeout int) error {
-	destHostname, err := net.LookupAddr(destIP.String())
-	if err != nil {
-		fmt.Printf("Doing traceroute to %s\n", destIP)
-	} else {
-		fmt.Printf("Doing traceroute to %s (%s)\n", destHostname[0], destIP)
-	}
 
-	if interfaceDevice == "" {
-		discoveredOutboundInterface, err := beacon.GetInterfaceDeviceFromDestIP(destIP)
-		if err != nil {
-			return fmt.Errorf("Failed to find an interface for %s: %s, explicitly provide an interface with -i", destIP.String(), err)
-		}
-		interfaceDevice = discoveredOutboundInterface
-	}
+	beacon.TracerouteBeacon(destIP, timeout, interfaceDevice)
 
-	tc, err := beacon.NewTransportChannel(
-		beacon.WithBPFFilter("icmp"),
-		beacon.WithInterface(interfaceDevice),
-	)
-	if err != nil {
-		return fmt.Errorf("Error creating transport channel: %s", err)
-	}
-	pc, err := tc.GetPathChannelTo(destIP, timeout)
-	if err != nil {
-		return err
-	}
+	// destHostname, err := net.LookupAddr(destIP.String())
+	// if err != nil {
+	// 	fmt.Printf("Doing traceroute to %s\n", destIP)
+	// } else {
+	// 	fmt.Printf("Doing traceroute to %s (%s)\n", destHostname[0], destIP)
+	// }
 
-	hopIdx := 1
-	for hop := range pc {
-		fmt.Printf("%d: ", hopIdx)
-		hopIdx++
+	// if interfaceDevice == "" {
+	// 	discoveredOutboundInterface, err := beacon.GetInterfaceDeviceFromDestIP(destIP)
+	// 	if err != nil {
+	// 		return fmt.Errorf("Failed to find an interface for %s: %s, explicitly provide an interface with -i", destIP.String(), err)
+	// 	}
+	// 	interfaceDevice = discoveredOutboundInterface
+	// }
 
-		if hop == nil {
-			fmt.Println("*")
-			continue
-		}
+	// tc, err := beacon.NewTransportChannel(
+	// 	beacon.WithBPFFilter("icmp"),
+	// 	beacon.WithInterface(interfaceDevice),
+	// )
+	// if err != nil {
+	// 	return fmt.Errorf("Error creating transport channel: %s", err)
+	// }
+	// pc, err := tc.GetPathChannelTo(destIP, timeout)
+	// if err != nil {
+	// 	return err
+	// }
 
-		hostname, err := net.LookupAddr(hop.String())
-		if err != nil {
-			fmt.Println(hop.String())
-		} else {
-			fmt.Printf("%s (%s)\n", hostname[0], hop.String())
-		}
-	}
+	// hopIdx := 1
+	// for hop := range pc {
+	// 	fmt.Printf("%d: ", hopIdx)
+	// 	hopIdx++
+
+	// 	if hop == nil {
+	// 		fmt.Println("*")
+	// 		continue
+	// 	}
+
+	// 	hostname, err := net.LookupAddr(hop.String())
+	// 	if err != nil {
+	// 		fmt.Println(hop.String())
+	// 	} else {
+	// 		fmt.Printf("%s (%s)\n", hostname[0], hop.String())
+	// 	}
+	// }
 
 	return nil
 }
---
+++ tracerouteBeacon.go
@@ -0,0 +1,61 @@
+package beacon
+
+import (
+	"fmt"
+	"net"
+
+	// "github.com/trstruth/beacon"
+)
+
+// Traceroute performs traditional traceroute
+func TracerouteBeacon(destIP net.IP, timeout int, interfaceDevice string) error {
+
+	fmt.Printf("Inside traceroute (beacon)")
+
+	destHostname, err := net.LookupAddr(destIP.String())
+	if err != nil {
+		fmt.Printf("Doing traceroute to %s\n", destIP)
+	} else {
+		fmt.Printf("Doing traceroute to %s (%s)\n", destHostname[0], destIP)
+	}
+
+	if interfaceDevice == "" {
+		discoveredOutboundInterface, err := GetInterfaceDeviceFromDestIP(destIP)
+		if err != nil {
+			return fmt.Errorf("Failed to find an interface for %s: %s, explicitly provide an interface with -i", destIP.String(), err)
+		}
+		interfaceDevice = discoveredOutboundInterface
+	}
+
+	tc, err := NewTransportChannel(
+		WithBPFFilter("icmp"),
+		WithInterface(interfaceDevice),
+	)
+	if err != nil {
+		return fmt.Errorf("Error creating transport channel: %s", err)
+	}
+	pc, err := tc.GetPathChannelTo(destIP, timeout)
+	if err != nil {
+		return err
+	}
+
+	hopIdx := 1
+	for hop := range pc {
+		fmt.Printf("%d: ", hopIdx)
+		hopIdx++
+
+		if hop == nil {
+			fmt.Println("*")
+			continue
+		}
+
+		hostname, err := net.LookupAddr(hop.String())
+		if err != nil {
+			fmt.Println(hop.String())
+		} else {
+			fmt.Printf("%s (%s)\n", hostname[0], hop.String())
+		}
+	}
+
+	return nil
+}
