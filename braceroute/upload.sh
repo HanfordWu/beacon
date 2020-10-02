7dedc591046b14bebb6dbff7d6b9151551b7e7f8
Tristan Struthers <trstruth@microsoft.com>
2020-10-02 20:06:11 +0000

    Remove upload script

--- braceroute/upload.sh
+++
@@ -1,5 +0,0 @@
-docker run --rm \
-    -v $GOPATH/src/github.com/trstruth/beacon:/beacon \
-    -v $HOME/.ssh:/root/.ssh \
-    arista-gopacket-compiler:braceroute && \
-scp braceroute netscript1_rw@stg30-0100-0001-02sw:/home/netscript1_rw/
