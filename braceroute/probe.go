aa5786909b5ba946c3b4a03e8b7caa5720045826
edwardbottom <edward.bottom@wustl.edu>
2021-01-14 11:29:54 -0800

    updated definition to have ip6 in filter

--- braceroute/probe.go
+++ braceroute/probe.go
@@ -75,7 +75,7 @@ func probeRun(cmd *cobra.Command, args []string) error {
 	fmt.Printf("%v\n", path)
 	stats := newProbeStats(path, numPackets, interfaceDevice)
 
-	fmt.Printf("Reading packet using BPF: %s\n", "(ip && ip[4:2]=0x6D) || (ip6 && ip[2:2] = 0x6D")
+	fmt.Printf("Reading packet using BPF: %s\n", "(ip && ip[4:2]=0x6D) || (ip6 && ip6[2:2] = 0x6D")
 	tc, err := beacon.NewBoomerangTransportChannel(
 		beacon.WithInterface(interfaceDevice),
 	)
